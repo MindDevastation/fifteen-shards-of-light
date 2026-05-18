641780d43dadf952d76254f3f0357cd8389e711a
Aleksey Pastukhov <42119796+MindDevastation@users.noreply.github.com>
2026-05-18 14:31:39 +0300

    Fix LevelPortal frame and inner trigger

--- scripts/core/level_portal.gd
+++ scripts/core/level_portal.gd
@@ -1,35 +1,51 @@
-extends Area3D
+extends Node3D
 
 @export var target_scene_path: String = ""
 
 var _is_active := false
 var _is_loading_scene := false
+var _frame_collision_shapes: Array[CollisionShape3D] = []
 
-@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var portal_trigger: Area3D = $PortalTrigger
+@onready var trigger_collision_shape: CollisionShape3D = $PortalTrigger/CollisionShape3D
 
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
+	_frame_collision_shapes = [
+		$LeftPost/CollisionShape3D,
+		$RightPost/CollisionShape3D,
+		$TopBeam/CollisionShape3D,
+	]
+	portal_trigger.body_entered.connect(_on_portal_trigger_body_entered)
 	_deactivate()
 
 
 func activate() -> void:
 	_is_active = true
 	show()
-	set_deferred("monitoring", true)
-	set_deferred("monitorable", true)
-	collision_shape.set_deferred("disabled", false)
+	_set_frame_collision_enabled(true)
+	_set_trigger_enabled(true)
 
 
 func _deactivate() -> void:
 	_is_active = false
 	hide()
-	set_deferred("monitoring", false)
-	set_deferred("monitorable", false)
-	collision_shape.set_deferred("disabled", true)
+	_set_trigger_enabled(false)
+	_set_frame_collision_enabled(false)
 
 
-func _on_body_entered(body: Node3D) -> void:
+func _set_trigger_enabled(is_enabled: bool) -> void:
+	portal_trigger.set_deferred("monitoring", is_enabled)
+	portal_trigger.set_deferred("monitorable", is_enabled)
+	trigger_collision_shape.set_deferred("disabled", not is_enabled)
+
+
+func _set_frame_collision_enabled(is_enabled: bool) -> void:
+	for frame_collision_shape in _frame_collision_shapes:
+		frame_collision_shape.set_deferred("disabled", not is_enabled)
+
+
+func _on_portal_trigger_body_entered(body: Node3D) -> void:
 	if not _is_active or _is_loading_scene:
 		return
 
