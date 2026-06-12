896694387bc0a15e5020bc8cb69dbf5e9b531891
Aleksey Pastukhov <42119796+MindDevastation@users.noreply.github.com>
2026-06-12 13:53:07 +0300

    Tune player camera and interactions

--- scripts/player/camera_controller.gd
+++ scripts/player/camera_controller.gd
@@ -8,16 +8,20 @@ extends Camera3D
 @export var max_zoom_distance: float = 10.0
 @export var zoom_step: float = 0.75
 @export var camera_height: float = 1.8
+@export_flags_3d_physics var camera_collision_mask: int = 1
+@export_range(0.05, 0.5, 0.01) var camera_collision_margin: float = 0.2
+@export_range(1.0, 40.0, 0.5) var camera_collision_smoothness: float = 18.0
 
 @onready var target: Node3D = get_node_or_null(target_path) as Node3D
 
-var _yaw: float = 0.0
+var _yaw: float = PI
 var _pitch: float = deg_to_rad(15.0)
-var _zoom_distance: float = 6.5
+var _zoom_distance: float = 2.6
 
 
 func _ready() -> void:
 	_zoom_distance = clamp(_zoom_distance, min_zoom_distance, max_zoom_distance)
+	_snap_to_orbit_position()
 	_update_mouse_mode()
 
 
@@ -52,9 +56,42 @@ func _process(_delta: float) -> void:
 	orbit_offset = orbit_offset.rotated(Vector3.RIGHT, _pitch)
 	orbit_offset = orbit_offset.rotated(Vector3.UP, _yaw)
 
-	global_position = target_focus + orbit_offset
+	var desired_position := target_focus + orbit_offset
+	var corrected_position := _get_collision_corrected_position(target_focus, desired_position)
+	global_position = global_position.lerp(corrected_position, clampf(camera_collision_smoothness * _delta, 0.0, 1.0))
 	look_at(target_focus, Vector3.UP)
 
+
+func _snap_to_orbit_position() -> void:
+	if target == null:
+		return
+
+	var target_focus := target.global_position + Vector3.UP * camera_height
+	var orbit_offset := Vector3(0.0, 0.0, _zoom_distance)
+	orbit_offset = orbit_offset.rotated(Vector3.RIGHT, _pitch)
+	orbit_offset = orbit_offset.rotated(Vector3.UP, _yaw)
+	global_position = _get_collision_corrected_position(target_focus, target_focus + orbit_offset)
+	look_at(target_focus, Vector3.UP)
+
+
+func _get_collision_corrected_position(target_focus: Vector3, desired_position: Vector3) -> Vector3:
+	var to_camera := desired_position - target_focus
+	if to_camera.length_squared() <= 0.000001:
+		return desired_position
+
+	var query := PhysicsRayQueryParameters3D.create(target_focus, desired_position)
+	query.collision_mask = camera_collision_mask
+	query.collide_with_areas = false
+	if target is CollisionObject3D:
+		query.exclude = [(target as CollisionObject3D).get_rid()]
+	var hit := get_world_3d().direct_space_state.intersect_ray(query)
+	if hit.is_empty():
+		return desired_position
+
+	var hit_position := hit["position"] as Vector3
+	var camera_direction := to_camera.normalized()
+	var corrected_distance := maxf(target_focus.distance_to(hit_position) - camera_collision_margin, camera_collision_margin)
+	return target_focus + camera_direction * corrected_distance
 
 func _update_mouse_mode() -> void:
 	if not current:
--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -5,21 +5,19 @@ const INTERACT_COOLDOWN_SECONDS := 0.1
 
 @export var walk_speed: float = 3.0
 @export var run_speed: float = 5.0
-@export var jump_velocity: float = 4.5
+@export var jump_velocity: float = 4.025
 @export var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export_range(1.0, 20.0, 0.1) var visual_turn_speed: float = 5.0
+@export_range(0.1, 0.6, 0.01) var max_step_height: float = 0.35
+@export_range(0.1, 0.8, 0.01) var step_check_distance: float = 0.35
+@export_range(1.0, 40.0, 0.5) var step_smoothness: float = 18.0
 
 @onready var visual_root: Node3D = $CharacterVisualRoot
 @onready var animation_controller: FoxHeroineAnimationController = $FoxHeroineAnimationController
 
 var _jump_cooldown_remaining: float = 0.0
 var _interact_cooldown_remaining: float = 0.0
-var _pending_interactable: Node = null
 
-
-func _ready() -> void:
-	if animation_controller != null and not animation_controller.one_shot_finished.is_connected(_on_animation_one_shot_finished):
-		animation_controller.one_shot_finished.connect(_on_animation_one_shot_finished)
 
 
 func _physics_process(delta: float) -> void:
@@ -62,7 +60,9 @@ func _physics_process(delta: float) -> void:
 	elif not animation_controller.is_dancing():
 		animation_controller.update_idle(delta)
 
+	var was_on_floor := is_on_floor()
 	move_and_slide()
+	_try_step_up(delta, movement_direction, was_on_floor)
 
 
 func _try_start_interaction() -> void:
@@ -73,11 +73,11 @@ func _try_start_interaction() -> void:
 	if interactable == null:
 		return
 
-	if not animation_controller.play_one_shot("cast_1"):
-		return
-
 	_interact_cooldown_remaining = INTERACT_COOLDOWN_SECONDS
-	_pending_interactable = interactable
+	if animation_controller != null:
+		animation_controller.play_one_shot("cast_1")
+	if interactable.has_method("interact"):
+		interactable.call("interact", self)
 
 
 func _find_best_interactable() -> Node:
@@ -100,22 +100,67 @@ func _find_best_interactable() -> Node:
 	return best
 
 
-func _on_animation_one_shot_finished(action_name: String) -> void:
-	if action_name != "cast_1":
+
+
+func _try_step_up(delta: float, movement_direction: Vector3, was_on_floor: bool) -> void:
+	if not was_on_floor or not is_on_floor():
+		return
+	if movement_direction.length_squared() <= 0.000001:
+		return
+	if max_step_height <= 0.0 or step_check_distance <= 0.0:
 		return
 
-	if _pending_interactable == null or not is_instance_valid(_pending_interactable):
-		_pending_interactable = null
+	var space_state := get_world_3d().direct_space_state
+	var direction := movement_direction.normalized()
+	var up := Vector3.UP
+	var floor_y := global_position.y
+	var exclude := [get_rid()]
+
+	var low_start := global_position + up * 0.08
+	var low_end := low_start + direction * step_check_distance
+	var low_query := PhysicsRayQueryParameters3D.create(low_start, low_end)
+	low_query.exclude = exclude
+	low_query.collide_with_areas = false
+	var low_hit := space_state.intersect_ray(low_query)
+	if low_hit.is_empty():
 		return
 
-	if _pending_interactable.has_method("can_player_interact") and not bool(_pending_interactable.call("can_player_interact", self)):
-		_pending_interactable = null
+	var high_start := global_position + up * (max_step_height + 0.12)
+	var high_end := high_start + direction * step_check_distance
+	var high_query := PhysicsRayQueryParameters3D.create(high_start, high_end)
+	high_query.exclude = exclude
+	high_query.collide_with_areas = false
+	if not space_state.intersect_ray(high_query).is_empty():
 		return
 
-	if _pending_interactable.has_method("interact"):
-		_pending_interactable.call("interact", self)
+	var top_probe_origin := global_position + direction * step_check_distance + up * (max_step_height + 0.25)
+	var top_probe_end := global_position + direction * step_check_distance + up * 0.02
+	var top_query := PhysicsRayQueryParameters3D.create(top_probe_origin, top_probe_end)
+	top_query.exclude = exclude
+	top_query.collide_with_areas = false
+	var top_hit := space_state.intersect_ray(top_query)
+	if top_hit.is_empty():
+		return
 
-	_pending_interactable = null
+	var top_position := top_hit["position"] as Vector3
+	var step_height := top_position.y - floor_y
+	if step_height <= 0.01 or step_height > max_step_height:
+		return
+
+	var top_normal := top_hit["normal"] as Vector3
+	if top_normal.dot(up) < cos(floor_max_angle):
+		return
+
+	var clearance_start := global_position + up * (max_step_height + 0.9)
+	var clearance_end := clearance_start + direction * step_check_distance
+	var clearance_query := PhysicsRayQueryParameters3D.create(clearance_start, clearance_end)
+	clearance_query.exclude = exclude
+	clearance_query.collide_with_areas = false
+	if not space_state.intersect_ray(clearance_query).is_empty():
+		return
+
+	var step_lerp := clampf(step_smoothness * delta, 0.0, 1.0)
+	global_position.y += lerpf(0.0, step_height, step_lerp)
 
 
 func _get_camera_relative_direction(input_direction: Vector2) -> Vector3:
